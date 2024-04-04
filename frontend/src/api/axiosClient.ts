82b08f374c8f948fb672adfb15e0de49b8dca646
DESKTOP-DF4I00S\waros <waroshi@gmail.com>
2024-04-05 01:59:55 +0900

    axios設定

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -4,7 +4,7 @@ import queryString from "query-string";
 // const BASE_URL = "http://localhost:3000/api"; //ruby on rails backend
 // const BASE_URL = "http://localhost:5000/api"; //typescript backend
 const BASE_URL =
-	"https://tsbackend-hx5ywpmpz-musoukuns-projects.vercel.app/api"; //typescript backend
+	"https://tsbackend-j9vsnopzo-musoukuns-projects.vercel.app/api"; //typescript backend
 
 const getToken = (): string | null => localStorage.getItem("token");
 
