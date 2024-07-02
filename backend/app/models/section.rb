3077da5e778d458591228721eb0abafd0633aecf
DESKTOP-DF4I00S\waros <waroshi@gmail.com>
2024-07-02 18:54:08 +0900

    backendと.vscodeを除外

--- backend/app/models/section.rb
+++
@@ -1,6 +0,0 @@
-# app/models/section.rb
-class Section < ApplicationRecord
-	belongs_to :memo
-	has_many :tasks, dependent: :destroy
-	# カラムに関するバリデーションや追加のロジックをここに記述
-end
--- backend/app/models/task.rb
+++
@@ -1,6 +0,0 @@
-# app/models/task.rb
-class Task < ApplicationRecord
-	belongs_to :section
-
-	# カラムに関するバリデーションや追加のロジックをここに記述
-end
