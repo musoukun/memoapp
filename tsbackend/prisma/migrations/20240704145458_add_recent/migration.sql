ed7bf166a295329ecf49be45b28cb4e68c142d06
DESKTOP-DF4I00S\waros <waroshi@gmail.com>
2024-07-06 19:30:35 +0900

    migration reset

--- tsbackend/prisma/migrations/20240704145458_add_recent/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Memo" ADD COLUMN     "accessCount" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "lastAccessedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ALTER COLUMN "title" SET DEFAULT '';
-
--- CreateIndex
-CREATE INDEX "index_memos_on_last_accessed_at" ON "Memo"("lastAccessedAt");
